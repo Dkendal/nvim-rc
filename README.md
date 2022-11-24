41c9119160cba32b31f09ac4d11002209c7f5983
Dylan Kendal <dylankendal@gmail.com>
2022-11-23 19:17:59 -0500

    feat: initial impl

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# nvim-rc
-direnv like lua rc file loading for Neovim
+# Nvim-RC
+
+Direnv-like lua rc file loading for Neovim.
+
+Loads `.nvimrc.lua` files in the current directory and all parent directories.
+
+If you're familiar with direnv, it works in a very similar manner by only loading trusted files that have been approved by the user.
+
+## Usage
+
+Create a `.nvimrc.lua` file in your project directory (or any parent directory).
+When you open Neovim or change directories, you'll receive a warning that the rc file is not allowed.
+Run `:RcAllow` to allow the file to be loaded.
+
+## Commands
+
+### `:RcAllow`
+
+Allow a `.nvimrc.lua` file to be sourced, this will save the checksum of the file to a file in the allow directory.
+If there are multiple files that are not yet approved you will be prompted to select one.
+Immediately sources the file after approval.
+
+### `:RcRevoke`
+
+Revoke a previous authorization.
+Prompts for selection if multiple rc files exist.
+
+### `:RcEdit`
+
+Open a rc file.
+Prompts for selection if multiple rc files exist.
+
+### `:RcReload`
+
+Reload all rc files.
+
+## Installation
+
+```lua
+vim.cmd.packadd 'nvim-rc'
+```
+
+Or use your favorite plugin manager.
+
+No other configuration is required.
---
+++ lua/nvim-rc.lua
@@ -0,0 +1,250 @@
+local M = {}
+
+local a = vim.api
+
+local f = vim.fn
+
+local rc_file_name = ".nvimrc.lua"
+
+M.config = {
+	allow_dir = f.stdpath("data") .. "/allow",
+}
+
+local function short(path)
+	return f.fnamemodify(path, ":~:.")
+end
+
+--- Like `vim.fn.findfile` but returns all matches.
+---@return string[]
+local function find_all_rc_files()
+	local path = f.getcwd() .. "/"
+
+	local acc = {}
+
+	repeat
+		local last_path = path
+		path = f.fnamemodify(path, ":h")
+		local file = path .. "/" .. rc_file_name
+
+		if f.filereadable(file) == 1 then
+			table.insert(acc, file)
+		end
+
+	until path == last_path
+
+	return f.reverse(acc)
+end
+
+M.find_all_rc_files = find_all_rc_files
+
+local function allow_file_path(checksum)
+	return M.config.allow_dir .. "/" .. checksum
+end
+
+local function print_deny_err(path)
+	vim.notify_once(
+		string.format("nvim-rc: %s is blocked. Run `:RcAllow` to allow its content", short(path)),
+		vim.log.levels.ERROR
+	)
+end
+
+--- Generate a checksum for a file.
+---@param file string
+---@return string
+local function gen_checksum(file)
+	local lines = f.systemlist("openssl sha256 " .. file)
+	local line = lines[1]
+	return vim.split(line, " ", { plain = true, trimempty = true })[2]
+end
+
+local function is_allowed(path)
+	local sum = gen_checksum(path)
+	local allow_file = allow_file_path(sum)
+
+	if f.filereadable(allow_file) == 1 then
+		-- Take the first line only
+		local contents = f.readfile(allow_file)[1]
+		if contents == path then
+			return true
+		else
+			return false
+		end
+	else
+		return false
+	end
+end
+
+local function denied_files()
+	local acc = {}
+	for _, path in ipairs(find_all_rc_files()) do
+		if not is_allowed(path) then
+			table.insert(acc, path)
+		end
+	end
+	return acc
+end
+
+local function allowed_files()
+	local acc = {}
+	for _, path in ipairs(find_all_rc_files()) do
+		if is_allowed(path) then
+			table.insert(acc, path)
+		end
+	end
+	return acc
+end
+
+local function source(path)
+	vim.notify_once("nvim-rc: loading " .. short(path), vim.log.levels.INFO)
+	dofile(path)
+end
+
+-- Find all .nvimrc.lua files from the current file to the root. For each
+-- check if the file matches a saved checksum, if the file does then source it,
+-- if it does not then terminate, and prompt the user that they need to
+-- run a command to allow the file for loading.
+function M.load_rc_files()
+	for _, path in ipairs(find_all_rc_files()) do
+		if is_allowed(path) then
+			source(path)
+		else
+			print_deny_err(path)
+		end
+	end
+end
+
+--- Allow a file to be sourced, this will save the checksum of the file
+--- to a file in the allow directory. Immediately sources the file after
+--- approval.
+function M.allow(args)
+	local choices = find_all_rc_files()
+
+	if #choices == 0 then
+		a.nvim_err_writeln("nvim-rc: no .nvimrc.lua files found")
+		return
+	end
+
+	if #choices == 1 then
+		M.allow_file(choices[1])
+		return
+	end
+
+	vim.ui.select(choices, {
+		prompt = "Select a file to allow",
+	}, function(selected)
+		if selected then
+			M.allow_file(selected)
+		end
+	end)
+end
+
+--- Allow a file to be sourced. Immediately source the file after approval.
+function M.allow_file(path)
+	local sum = gen_checksum(path)
+	local allow_file = allow_file_path(sum)
+
+	f.writefile({ path }, allow_file)
+	source(path)
+end
+
+--- List all allowed checksums.
+function M.ls()
+	vim.pretty_print(f.systemlist("ls -C " .. M.config.allow_dir))
+end
+
+function M.edit()
+	local choices = find_all_rc_files()
+
+	if #choices == 0 then
+		vim.cmd.edit(rc_file_name)
+		return
+	end
+
+	if #choices == 1 then
+		vim.cmd.edit(choices[1])
+		return
+	end
+
+	vim.ui.select(choices, {
+		prompt = "Select a file to edit",
+	}, function(selected)
+		if selected then
+			vim.cmd.edit(selected)
+		end
+	end)
+end
+
+--- Revoke a previous authorization.
+function M.revoke()
+	local choices = allowed_files()
+
+	if #choices == 0 then
+		a.nvim_err_writeln("nvim-rc: no .nvimrc.lua files found")
+		return
+	end
+
+	if #choices == 1 then
+		M.revoke_file(choices[1])
+		return
+	end
+
+	vim.ui.select(choices, {
+		prompt = "Select a file to revoke",
+	}, function(selected)
+		if selected then
+			M.revoke_file(selected)
+		end
+	end)
+end
+
+--- Deny a file from being sourced.
+function M.revoke_file(path)
+	local sum = gen_checksum(path)
+	local allow_file = allow_file_path(sum)
+
+	if f.filereadable(allow_file) == 1 then
+		f.delete(allow_file)
+		vim.notify_once("nvim-rc: " .. short(path) .. " revoked", vim.log.levels.INFO)
+	end
+end
+
+--- Reload all .nvimrc.lua files.
+function M.reload()
+	for _, path in ipairs(allowed_files()) do
+		source(path)
+	end
+end
+
+function M.setup()
+	f.mkdir(M.config.allow_dir, "p")
+
+	local group = a.nvim_create_augroup("nvim-rc", { clear = true })
+
+	a.nvim_create_user_command("RcAllow", M.allow, {
+		nargs = "*",
+	})
+
+	a.nvim_create_user_command("RcEdit", M.edit, {
+		nargs = "*",
+	})
+
+	a.nvim_create_user_command("RcReload", M.reload, {
+		nargs = "*",
+	})
+
+	a.nvim_create_user_command("RcRevoke", M.revoke, {
+		nargs = "*",
+	})
+
+	a.nvim_create_user_command("RcLs", M.ls, {
+		nargs = "*",
+	})
+
+	a.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
+		group = group,
+		pattern = "*",
+		callback = M.load_rc_files,
+	})
+end
+
+return M
